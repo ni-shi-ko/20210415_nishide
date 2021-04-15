991453736b9b98ea17e42fcd0de3803478cc3795
”こうしろう” <nishideko0824@gmail.com>
2021-04-15 17:36:24 +0900

    first-commit

---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <input type="number" v-model="zipcode">
+    <button @click="searchAddress">住所自動入力</button>
+    <p>Address: {{address}}</p>
+   
+  </div>
+</template>
+
+<script>
+  
+</script>
